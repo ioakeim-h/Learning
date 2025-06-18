d412b9bef3ed2bb52415eba1e0154d3c44f2997c
Ioakeim Hadjimpalasis <ioakeim.h@gmail.com>
2025-06-18 09:48:04 +0300

    Delete Data-Science-Infinity directory

--- Data-Science-Infinity/Machine Learning/Data Preparation/401_Feature_Selection_Correlation_Matrix.py
+++
@@ -1,8 +0,0 @@
-
-# Feature Selection using a simple Correlation Matrix
-
-import pandas as pd
-df = pd.read_csv("feature_selection_sample_data.csv")
-
-# Correlation matrix
-correlation_matrix = df.corr()
--- Data-Science-Infinity/README.md
+++
@@ -1,3 +0,0 @@
-# Data-Science-Infinity
-
-Find out more about this course and its curriculum by clicking <a href="https://www.data-science-infinity.com/">here</a>.
